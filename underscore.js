8709bfc1da67037b4cca7eeee7a53150dd2a992c
Daniel Streit <Daniel.Streit@gmail.com>
2014-08-10 14:17:14 -0700

    Optimize isEmpty

--- underscore.js
+++ underscore.js
@@ -1069,7 +1069,7 @@
   _.isEmpty = function(obj) {
     if (obj == null) return true;
     if (_.isArray(obj) || _.isString(obj) || _.isArguments(obj)) return obj.length === 0;
-    for (var key in obj) if (_.has(obj, key)) return false;
+    for (var key in obj) if (obj.hasOwnProperty(key)) return false;
     return true;
   };
 
